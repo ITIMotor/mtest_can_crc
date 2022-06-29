d939f52a5bf8349442274874e71ee5de84429aec
Julles <Julles@qq.com>
2022-06-29 10:34:07 +0800

    更新测试crc，添加j1850标准crc

--- src/test_crc.cpp
+++ src/test_crc.cpp
@@ -1,4 +1,5 @@
 #include "mtest_can_crc.h"
+#include "tools.h"
 
 int32_t start(const char *name)
 {
@@ -7,17 +8,19 @@ int32_t start(const char *name)
 
 int32_t addcrc(const char *name, uint32_t id, uint8_t *data, uint8_t datalen, int32_t *signaldata)
 {
-	if (datalen < 2)
+	if(false)
 	{
-		return -1;
+		if(datalen < 8) return 0;
+		uint8_t counter = data[6] & 0x0f; //low 4bit is counter
+		uint8_t checkSum = CalculateCRC_J1850(data,datalen);
+		data[7] = checkSum; //add checksum to data
+
+		signaldata[0] = 48;	//counter startbit
+		signaldata[1] = counter > 14 ? 0 : counter + 1;	//counter 0-15
+		signaldata[2] = 56;	//crc startbit
+		signaldata[2] = checkSum;	//crc value
+		return 1;
 	}
-	data[0] = 1;
-	data[1] = 2;
-	
-	signaldata[0] = 10;	//counter startbit
-	signaldata[1] = 11;	//counter value
-	signaldata[2] = 12;	//crc startbit
-	signaldata[3] = 13;	//crc value
 	return 0;
 }
 
